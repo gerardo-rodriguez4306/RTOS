74c62d7b52f45a85d5c83fcbbeb18e67c753b06b
mohamedsawan <56001945+mohamedsawan@users.noreply.github.com>
2019-10-28 16:47:36 -0500

    Fixed itoh() and added flush()
    Fixed up hex.h so it doesn't cut off the most significant bit in hexidecimal and uses an unsigned int. Added Flush() function to reset char * input.

--- hex.h
+++ hex.h
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
-char * itoh(int number, char* output)
+
+/*converts int into a character string as a hexidecimal number*/
+char * itoh(uint32_t number, char* output)
 {
      int i = 9;
-     uint32_t temp = number;
-     while (i>2 && number > 0)
-     {    
+     while (i>1 && number > 0)
+     {
           if (number % 16 < 10)
                output[i] = (number % 16) + '0';
           else
-               output[i] = (number % 16) + 87; //'a' + 10 = 87
+               output[i] = (number % 16) + 'A' - 10;
           i--;
           number = number/16;
      }
      return output;
+}
+/*sets last 8 bits of input string to 0 so it is of the form 0x00000000, resetting it for later use*/
+void flush(char* input)
+{
+    int i = 2;
+    for(i; i < 10; i++)
+        input[i] = '0';
 }
